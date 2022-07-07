f2e50f3c75e9a1dcb9daa67f5c223bddd3d6f999
Alex Tonenchuk <25725at@gmail.com>
2022-07-07 21:31:25 +0300

    Add hendler of errors in Thunk

--- src/thunk/fetchBooks.js
+++ src/thunk/fetchBooks.js
@@ -4,27 +4,30 @@ import { togleLoading } from "../slices/loadingSlice";
 
 
 export const fetchBooks = (search, categorie, sort, buttonType) => (dispatch, getState) => {
-  dispatch(togleLoading());
   let url;
   const startIndex = ( buttonType==='search'?0:getState().books.value.length );
-  if ( categorie==='all' ) {
+  if (categorie==='all') {
     url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&orderBy=${sort}&startIndex=${startIndex}&maxResults=30`
   } else {
     url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}+subject:${categorie}&orderBy=${sort}&startIndex=${startIndex}&maxResults=30`
-  }
+  };
+  dispatch(togleLoading());
   fetch(url)
   .then((response) => {
+    if (!response.ok){
+      throw new Error('response.status is not ok !!!')
+    };
     return response.json();
   })
   .then((data) => {
     if (buttonType==='search'){
       dispatch(loadBooks(data.items));
       dispatch(changeFound(data.totalItems));
-      dispatch(togleLoading());
     } else {
         dispatch(loadMore(data.items));
-        dispatch(togleLoading());
     }
   })
+  .catch((error) => alert(error))
+  .finally(() => dispatch(togleLoading()))
 }
   
