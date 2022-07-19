ffa7fc876950e061204281f29a05a4bc526de3be
Alex Tonenchuk <25725at@gmail.com>
2022-07-19 06:11:38 +0300

    Delet repeat code in books reduser

--- src/slices/booksSlice.js
+++ src/slices/booksSlice.js
@@ -8,16 +8,7 @@ export const booksSlice = createSlice({
   reducers: {
     loadBooks: (state, action) => {
       state.value = [];
-      function concatWithoutRepeat (arr1, arr2=[]) {
-        let arr3 = [...arr1];
-          arr2.forEach((iArr2) => {
-              if (arr3.find((iArr1) => {
-                return (iArr1.id===iArr2.id)
-              })) { } else {arr3.push(iArr2)}
-          });
-        return arr3
-      };
-      state.value = concatWithoutRepeat(state.value, action.payload);
+      state.value = this.concatWithoutRepeat(state.value, action.payload);
     },
     loadMore: (state, action) => {
       function concatWithoutRepeat (arr1, arr2=[]) {
--- src/thunk/fetchBooks.js
+++ src/thunk/fetchBooks.js
@@ -6,12 +6,17 @@ import { togleLoading } from "../slices/loadingSlice";
 export const fetchBooks = (search, categorie, sort, buttonType) => (dispatch, getState) => {
   const startIndex = buttonType==='search' ? 0 : getState().books.value.length;
   const subject = categorie==='all' ? '' : `subject:${categorie}`;
-  const url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}+${subject}&orderBy=${sort}&startIndex=${startIndex}&maxResults=30`;
+  const url = `https://www.googleapis.com/books/v1/volumes?
+              q=intitle:${search}
+              +${subject}
+              &orderBy=${sort}
+              &startIndex=${startIndex}
+              &maxResults=30`;
   dispatch(togleLoading());
   fetch(url)
   .then((response) => {
     if (!response.ok){
-      throw new Error('ERROR: response.status is not ok !!!')
+      throw new Error('ERROR: response.status is not ok.')
     };
     return response.json();
   })
